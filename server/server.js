aa58d622827110e03182750a03d00df6fcf334d4
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-03-08 00:40:50 -0600

    access control allow origin

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const http = require('http');
 const port = 5000;
 
 http.createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all domains
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+
     if (req.url === '/data') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: "Hello from the backend!" }));
