1bd99a5ac9eccc1aa42097745e000b2528042e26
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-03-07 23:56:45 -0600

    final

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = 5000;
 http.createServer((req, res) => {
     if (req.url === '/data') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: "Hello from the backend! Mustafa did it!" }));
+        res.end(JSON.stringify({ message: "Hello from the backend!" }));
     } else {
         res.writeHead(404);
         res.end();
