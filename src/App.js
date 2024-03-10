c4b15509d9a56a47b0049e91bb211e420b8c960f
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-03-10 16:16:19 -0500

    post office frontend

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import LoginScreen from './pages/login';
+import Registration from './pages/register';
+import Package from './pages/sendPackage';
+import "./css/App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app-container"> {/* Wrap header and routes in a container */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<About />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/Send Package" element={<Package />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
